d3e60d3538e87105883ba79f7384ad96855a7448
bsudekum <bobby@mapbox.com>
2015-09-15 15:13:50 -0700

    Add lint check, allow for onMouseMove as passed prop

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-'use strict'
+'use strict';
 
 var React = require('react');
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 var Range = React.createClass({
   displayName: 'Range',
+  propTypes: {
+    onChange: React.PropTypes.func,
+    onMouseMove: React.PropTypes.func
+  },
   getDefaultProps: function() {
     return {
       type: 'range'
-    }
+    };
   },
-  onSlideChange: function(e) {
+  onRangeChange: function(e) {
+    if (this.props.onMouseMove) this.props.onMouseMove(e);
     if (e.buttons !== 1) return;
-    this.props.onChange(e);
+    if (this.props.onChange) this.props.onChange(e);
   },
   componentWillReceiveProps: function(props) {
     React.findDOMNode(this).value = props.value;
@@ -20,14 +25,14 @@ var Range = React.createClass({
   render: function() {
     var props = _extends({}, this.props, {
       defaultValue: this.props.value,
-      onMouseMove: this.onSlideChange,
+      onMouseMove: this.onRangeChange,
       onChange: function() {}
     });
     delete props.value;
     return React.createElement(
       'input',
       props
-    )
+    );
   }
 });
 
